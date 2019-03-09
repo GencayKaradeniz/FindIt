3e975e4c0e0db1168b29ea7900894a6a6bb61f0c
GencayKaradeniz <gencay.karadeniz.gk@gmail.com>
2019-03-10 02:00:53 +0300

    filter page done

---
+++ FindIt/Controllers/FilterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindIt.Controllers
+{
+    public class FilterController : Controller
+    {
+        // GET: Filter
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FilterPage()
+        {
+            return View();
+        }
+    }
+}
