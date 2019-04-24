3a539027e0e4d9d755aa49bb378b21a61f228dc0
GencayKaradeniz <gencay.karadeniz.gk@gmail.com>
2019-04-25 00:26:27 +0300

    map page design finished

---
+++ FindIt/Controllers/MapController.cs
@@ -0,0 +1,32 @@
+﻿using FindIt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindIt.Controllers
+{
+    public class MapController : Controller
+    {
+        FinditDb db = new FinditDb();
+        // GET: Map
+        [HttpGet]
+        public ActionResult MapDetails()
+        {
+            var shelfs = new List<SelectListItem>();
+
+            db.tbl_Raflar.OrderBy(s => s.Raf_Ad).ToList().ForEach(s => shelfs.Add(new SelectListItem
+            {
+                Text = s.Raf_Ad,
+                Value = s.Raf_ID.ToString()
+            }));
+
+            var model = new ProductOperationsLists
+            {
+                Shelfs = shelfs
+            };
+            return View(model);
+        }
+    }
+}
